c49cf0e488722c90e11fdd6fb92b95e1c24a23ff
superzilong <zilong919@163.com>
2019-09-10 09:01:30 +0800

    Implement controller layer for ARTICLE CRUD API.

---
+++ src/main/java/me/zilong/simpleblog/controller/ArticleController.java
@@ -0,0 +1,50 @@
+package me.zilong.simpleblog.controller;
+
+import me.zilong.simpleblog.data.Article;
+import me.zilong.simpleblog.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/articles")
+public class ArticleController {
+
+    @Autowired
+    ArticleService articleSrv;
+
+    @GetMapping
+    public List<Article> getArticleList() {
+        List<Article> list = articleSrv.getAllArticle();
+        return list;
+    }
+
+    @PostMapping
+    public String postArticle(Article article) {
+        articleSrv.addNewArticle(article);
+        return "success";
+    }
+
+    @GetMapping(value="/{id}")
+    public Article getArticle(@PathVariable Long id) {
+        return articleSrv.getArticleById(id);
+    }
+
+    @PutMapping(value="/{id}")
+    public String putArticle(@PathVariable Long id, Article article) {
+        Article a = articleSrv.getArticleById(id);
+        a.setTitle(article.getTitle());
+        a.setMdContent(article.getMdContent());
+        a.setHtmlContent(article.getHtmlContent());
+        a.setSummary(article.getSummary());
+        articleSrv.modifyArticle(a);
+        return "success";
+    }
+
+    @DeleteMapping(value="/{id}")
+    public String deleteArticle(@PathVariable Long id) {
+        articleSrv.deleteArticleById(id);
+        return "success";
+    }
+}
--- src/main/java/me/zilong/simpleblog/service/ArticleService.java
+++ src/main/java/me/zilong/simpleblog/service/ArticleService.java
@@ -6,16 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
 @Service
 @Transactional
 public class ArticleService {
     @Autowired
     ArticleRepository articleRepo;
 
-    public int addNewArticle(Article article) {
+    public String addNewArticle(Article article) {
         articleRepo.save(article);
-        return 0;
+        return "success";
     }
 
+    public List<Article> getAllArticle() {
+        List<Article> res = Lists.newArrayList(articleRepo.findAll());
+        return res;
+    }
 
+    public Article getArticleById (Long id)
+    {
+        return articleRepo.findById(id).get();
+    }
+
+    public String modifyArticle(Article article)
+    {
+        articleRepo.save(article);
+        return "success";
+    }
+
+    public String deleteArticleById(Long id)
+    {
+        articleRepo.deleteById(id);
+        return "success";
+    }
 }
